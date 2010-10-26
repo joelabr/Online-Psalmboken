28ff05e8e529a09ba2b4c9233dd1c9ed330f53d4
Daniel Abrahamsson <hamsson@gmail.com>
2010-10-26 18:01:56 +0200

    Give up digest authentication (appearantly not supported on our current server) and user basic (bleh)

--- admin/authentication.php
+++ admin/authentication.php
@@ -3,49 +3,16 @@ require('config.php'); //In this file you define $ADMIN_USERS
 
 $realm = "onlinepsalmboken";
 
-//Parse headers directly as PHP_AUTH_DIGEST is not properly set.
-$headers = apache_request_headers();
-if(isset($headers['Authorization']))
-  $digest_data = $headers['Authorization'];
-  
-if (empty($digest_data)) {
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.
-           '",qop="auth",nonce="'.uniqid('ps').'",opaque="'.md5($realm).'"');
-
-    die('Access denied');
-}
-
-// analyze digest data
-if (!($data = http_digest_parse($digest_data)) ||
-    !isset($ADMIN_USERS[$data['username']]))
-    die('Wrong Credentials!');
-
-
-// generate the valid response
-$A1 = md5($data['username'] . ':' . $realm . ':' . $ADMIN_USERS[$data['username']]);
-$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
-$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
-
-if ($data['response'] != $valid_response)
-    die('Wrong Credentials!');
-
-// function to parse the http auth header
-function http_digest_parse($txt)
-{
-    // protect against missing data
-    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
-    $data = array();
-    $keys = implode('|', array_keys($needed_parts));
-
-    preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
-
-    foreach ($matches as $m) {
-        $data[$m[1]] = $m[3] ? $m[3] : $m[4];
-        unset($needed_parts[$m[1]]);
-    }
-
-    return $needed_parts ? false : $data;
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    die('No access');
+} else {
+  if(!isset($ADMIN_USERS[$_SERVER['PHP_AUTH_USER']]) ||
+      $_SERVER['PHP_AUTH_PW'] != $ADMIN_USERS[$_SERVER['PHP_AUTH_USER']])
+  {
+    die("Wrong credentials!");
+  }
 }
 
 ?>
